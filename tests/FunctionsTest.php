e4c1702e9e4ea803c12dfa4d04742d48ee3dc05f
priyabrata das <pdas544@gmail.com>
2025-07-19 22:45:14 +0530

    modify FunctionsTest.php to use different tests for testing diff aspects

--- tests/FunctionsTest.php
+++ tests/FunctionsTest.php
@@ -5,7 +5,11 @@ declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 require __DIR__ . '/../lib/functions.php';
 class FunctionsTest extends TestCase{
-    public function testAdd(){
+    public function testAdd(): void{
         $this->assertSame(3,add(1,2));
+    }
+
+    public function testAddTwoNegativeNumbers(): void{
+        $this->assertSame(-2,add(-1,-1));
     }
 }
