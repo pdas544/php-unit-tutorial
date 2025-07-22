746cb89cce2d320a07b1749175ea303e346c88ac
priyabrata das <pdas544@gmail.com>
2025-07-22 22:46:31 +0530

    modify PersonTest to use Test Attribute and markTestIncomplete() method

--- tests/PersonTest.php
+++ tests/PersonTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use App\Person;
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\Test;
 
 class PersonTest extends TestCase{
     public function testGetFullName(){
@@ -11,5 +12,13 @@ class PersonTest extends TestCase{
         $person->setFirstName('John');
         $person->setLastName('Doe');
         $this->assertSame('John Doe', $person->getFullName());
+    }
+
+    #[Test]
+    public function FullNameWithoutSurname(){
+//        $person = new Person();
+//        $person->setFirstName('John');
+//        $this->assertSame('John', $person->getFullName());
+        $this->markTestIncomplete('It is not implemented yet');
     }
 }
