cf01f50687d8c9239b7dc470895ecb14ff435950
priyabrata das <pdas544@gmail.com>
2025-07-31 19:10:50 +0530

    use depends attribute

--- tests/QueueTest.php
+++ tests/QueueTest.php
@@ -3,16 +3,29 @@ declare(strict_types=1);
 
 use App\Queue;
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\Depends;
 
 final class QueueTest extends TestCase{
-    public function testNewQueueIsEmpty(){
+    public function testNewQueueIsEmpty(): Queue{
         $queue = new Queue();
         $this->assertSame(0, $queue->getSize());
+
+        return $queue;
     }
 
-    public function testEnqueue(){
-        $queue = new Queue();
+    #[Depends('testNewQueueIsEmpty')]
+    public function testEnqueue(Queue $queue): Queue{
+
         $queue->enqueue('John');
         $this->assertSame(1, $queue->getSize());
+
+        return $queue;
+    }
+
+    #[Depends('testEnqueue')]
+    public function testDequeueAndReturn(Queue $queue): void{
+
+        $this->assertSame('John', $queue->dequeue());
+
     }
 }
